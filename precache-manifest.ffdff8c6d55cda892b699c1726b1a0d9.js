dc6f603ec6b9a872cf619ca3b44487fc181dca0c
Akshat <75172495+Akshat-cs@users.noreply.github.com>
2023-06-09 17:48:51 +0530

    Updates

---
+++ precache-manifest.ffdff8c6d55cda892b699c1726b1a0d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e65f31156a3e7dd4319",
+    "url": "/TextUtils-React/static/css/main.a1bacf59.chunk.css"
+  },
+  {
+    "revision": "3e65f31156a3e7dd4319",
+    "url": "/TextUtils-React/static/js/main.3e65f311.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "2591e466b5f270c0c477",
+    "url": "/TextUtils-React/static/js/runtime~main.2591e466.js"
+  },
+  {
+    "revision": "9f8cd87a4681699068f6b2adc7dc737d",
+    "url": "/TextUtils-React/index.html"
+  }
+];
